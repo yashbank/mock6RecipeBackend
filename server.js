8f40a3bd9b180988ebc11bc901a2f7d8dcc8c2da
yashbank <yashbank2002@gmail.com>
2023-03-21 11:48:31 +0530

    done deploy

---
+++ server.js
@@ -0,0 +1,11 @@
+// server.js
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+const PORT = process.env.PORT || 6500
+
+
+server.use(middlewares)
+server.use(router)
+server.listen(PORT)
